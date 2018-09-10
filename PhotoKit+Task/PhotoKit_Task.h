62651090c753ed4ad2d35113c51b30372fdfe0f3
hitoshiabe <a.h.0829.63@gmail.com>
2018-09-10 17:41:41 +0900

    add project

---
+++ PhotoKit+Task/PhotoKit_Task.h
@@ -0,0 +1,19 @@
+//
+//  PhotoKit_Task.h
+//  PhotoKit+Task
+//
+//  Created by 阿部 仁史 on 2018/09/10.
+//  Copyright © 2018年 hitoshiabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoKit_Task.
+FOUNDATION_EXPORT double PhotoKit_TaskVersionNumber;
+
+//! Project version string for PhotoKit_Task.
+FOUNDATION_EXPORT const unsigned char PhotoKit_TaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoKit_Task/PublicHeader.h>
+
+
